17522614d84916217d2c23d408e73fd20d626721
Petko <petkotihomirovpetkov@gmail.com>
2017-11-30 15:04:30 +0200

    Chaning the name of the project

---
+++ ExamPreparation/ExamPreparation/FillTheMatrix/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FillTheMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First Task
+
+            int n = Int32.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+        }
+    }
+}
