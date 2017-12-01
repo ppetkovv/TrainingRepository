e6af4355bb4c242640df4f6c9bf083d563390f1f
Petko <petkotihomirovpetkov@gmail.com>
2017-12-01 19:16:36 +0200

    Added the tasks BitShiftMatrix and Kitty

---
+++ SecondExamPreparation/SecondExamPreparation/BitShiftMatrix/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Numerics;
+using System.Linq;
+
+namespace SecondExamPreparation
+{
+    class BitShiftMatrix
+    {
+        public static void Main(string[] args)
+        {
+            int rowsCount = Int32.Parse(Console.ReadLine());
+            int colsCount = Int32.Parse(Console.ReadLine());
+
+            BigInteger[,] matrix = new BigInteger[rowsCount, colsCount];
+
+            int mainCounter = -1;
+            int currentCounter = 0;
+            for (int row = matrix.GetLength(0) - 1; row >= 0; row--)
+            {
+                mainCounter++;
+                currentCounter = mainCounter;
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = (BigInteger)Math.Pow(2, currentCounter);
+                    currentCounter++;
+                }
+            }
+
+
+            int movesCount = Int32.Parse(Console.ReadLine());
+            int[] moves = Console.ReadLine().Split().Select(Int32.Parse).ToArray();
+            int coef = Math.Max(rowsCount, colsCount);
+            int targetRow = 0;
+            int targetCol = 0;
+            int currentRow = matrix.GetLength(0) - 1;
+            int currentCol = 0;
+            BigInteger sum = 0;
+
+            for (int i = 0; i < movesCount; i++)
+            {
+                targetRow = moves[i] / coef;
+                targetCol = moves[i] % coef;
+
+                if (targetCol < currentCol)
+                {
+                    for (int colIndex = currentCol; colIndex >= targetCol; colIndex--)
+                    {
+                        sum += matrix[currentRow, colIndex];
+                        matrix[currentRow, colIndex] = 0;
+                    }
+                }
+                else
+                {
+                    for (int colIndex = currentCol; colIndex <= targetCol; colIndex++)
+                    {
+                        sum += matrix[currentRow, colIndex];
+                        matrix[currentRow, colIndex] = 0;
+                    }
+                }
+                currentCol = targetCol;
+
+                if (targetRow < currentRow)
+                {
+                    for (int rowIndex = currentRow; rowIndex >= targetRow; rowIndex--)
+                    {
+                        sum += matrix[rowIndex, currentCol];
+                        matrix[rowIndex, currentCol] = 0;
+                    }
+                }
+                else
+                {
+                    for (int rowIndex = currentRow; rowIndex <= targetRow; rowIndex++)
+                    {
+                        sum += matrix[rowIndex, currentCol];
+                        matrix[rowIndex, currentCol] = 0;
+                    }
+                }
+                currentRow = targetRow;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ SecondExamPreparation/SecondExamPreparation/Kitty/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] elements = Console.ReadLine().ToArray();
+            int[] moves = Console.ReadLine().Split().Select(Int32.Parse).ToArray();
+
+            int currentMove = 0;
+            int currentIndex = 0;
+
+            int soulsCount = 0;
+            int foodsCount = 0;
+            int deadLocks = 0;
+
+            switch (elements[currentIndex])
+            {
+                case '@':
+                    elements[currentIndex] = '\u0000';
+                    soulsCount++;
+                    break;
+                case '*':
+                    elements[currentIndex] = '\u0000';
+                    foodsCount++;
+                    break;
+                case 'x':
+                    deadLocks++;
+                    if (currentIndex % 2 == 0)
+                    {
+                        if (soulsCount - 1 < 0)
+                        {
+                            Console.WriteLine(string.Format("You are deadlocked, you greedy kitty!\nJumps before deadlock: {0}", 0));
+                            return;
+                        }
+                        else
+                        {
+                            soulsCount--;
+                            elements[currentIndex] = '@';
+                        }
+                    }
+                    else
+                    {
+                        if (foodsCount - 1 < 0)
+                        {
+                            Console.WriteLine(string.Format("You are deadlocked, you greedy kitty!\nJumps before deadlock: {0}", 0));
+                            return;
+                        }
+                        else
+                        {
+                            foodsCount--;
+                            elements[currentIndex] = '*';
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            for (int i = 0; i < moves.Length; i++)
+            {
+                currentMove = moves[i];
+                currentIndex += currentMove;
+
+                while (currentIndex < 0)
+                {
+                    currentIndex = elements.Length + currentIndex;
+                }
+                while (currentIndex > elements.Length - 1)
+                {
+                    currentIndex = currentIndex - elements.Length;
+                }
+
+                switch (elements[currentIndex])
+                {
+                    case '@':
+                        elements[currentIndex] = '\u0000';
+                        soulsCount++;
+                        break;
+                    case '*':
+                        elements[currentIndex] = '\u0000';
+                        foodsCount++;
+                        break;
+                    case 'x':
+                        deadLocks++;
+                        if (currentIndex % 2 == 0)
+                        {
+                            if (soulsCount - 1 < 0)
+                            {
+                                Console.WriteLine(string.Format("You are deadlocked, you greedy kitty!\nJumps before deadlock: {0}", i + 1));
+                                return;
+                            }
+                            else
+                            {
+                                soulsCount--;
+                                elements[currentIndex] = '@';
+                            }
+                        }
+                        else
+                        {
+                            if (foodsCount - 1 < 0)
+                            {
+                                Console.WriteLine(string.Format("You are deadlocked, you greedy kitty!\nJumps before deadlock: {0}", i + 1));
+                                return;
+                            }
+                            else
+                            {
+                                foodsCount--;
+                                elements[currentIndex] = '*';
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(string.Format("Coder souls collected: {0}\nFood collected: {1}\nDeadlocks: {2}", soulsCount, foodsCount, deadLocks));
+        }
+    }
+}
