cdd803ec51efa1d3d9b48811c6a1abc301a8d0eb
Petko <petkotihomirovpetkov@gmail.com>
2018-01-15 00:08:56 +0200

    Submitting Coki Skoki and Message in Bottle

---
+++ DSAWorkshop/DSAWorkshop/CokiSkoki/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CokiSkoki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Int32.Parse(Console.ReadLine());
+            List<int> buildings = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToList();
+            if (buildings.Count != N)
+                return;
+
+            List<int> lengthOfSequences = new List<int>();
+            int currentSequence = 0;
+            int currentMax = 0;
+            for (int i = 0; i < buildings.Count; i++)
+            {
+                currentSequence = 0;
+                currentMax = buildings[i];
+                for (int j = i + 1; j < buildings.Count; j++)
+                {
+                    if (buildings[j] > currentMax)
+                    {
+                        currentSequence++;
+                        currentMax = buildings[j];
+                    }
+                }
+                lengthOfSequences.Add(currentSequence);
+            }
+
+            Console.WriteLine(lengthOfSequences.Max() + "\n" + string.Join(" ", lengthOfSequences));
+        }
+    }
+}
---
+++ DSAWorkshop/DSAWorkshop/MessagesInBottle/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MessagesInBottle
+{
+    class Program
+    {
+        private static string secretMessageAsString;
+        private static Dictionary<string, char> dictionary;
+        private static int finalCount = 0;
+        private static List<string> decodedMessages;
+
+        static void Main(string[] args)
+        {
+            secretMessageAsString = Console.ReadLine();
+            string cipherAsString = Console.ReadLine();
+
+            string[] cipher = Regex.Split(cipherAsString, "([A-Z][0-9]{1,})").Where(x => x != string.Empty).ToArray();
+            dictionary = new Dictionary<string, char>();
+            for (int i = 0; i < cipher.Length; i++)
+            {
+                dictionary.Add(cipher[i].Substring(1), cipher[i][0]);
+            }
+
+
+            decodedMessages = new List<string>();
+            DecodeMessage(0, new StringBuilder());
+
+            Console.WriteLine(finalCount);
+            if (decodedMessages.Count != 0)
+                Console.WriteLine(string.Join("\n", decodedMessages.OrderBy(x => x)));
+        }
+
+        private static void DecodeMessage(int currentPosition, StringBuilder currentMessage)
+        {
+            for (int usedDigits = 1; usedDigits <= secretMessageAsString.Length; usedDigits++)
+            {
+                if (currentPosition + usedDigits > secretMessageAsString.Length)
+                {
+                    if (currentPosition == secretMessageAsString.Length)
+                    {
+                        decodedMessages.Add(currentMessage.ToString());
+                        finalCount++;
+                    }
+                    if (currentMessage.Length != 0)
+                    {
+                        int tempValue = currentPosition >= currentMessage.Length ? currentMessage.Length - 1 : currentPosition;
+                        currentMessage = currentMessage.Remove(tempValue, currentMessage.Length - tempValue);
+                    }
+                    return;
+                }
+                if (!dictionary.ContainsKey(secretMessageAsString.Substring(currentPosition, usedDigits)))
+                {
+                    continue;
+                }
+                currentMessage.Append(dictionary[secretMessageAsString.Substring(currentPosition, usedDigits)]);
+                DecodeMessage(currentPosition + usedDigits, currentMessage);
+            }
+        }
+    }
+}
