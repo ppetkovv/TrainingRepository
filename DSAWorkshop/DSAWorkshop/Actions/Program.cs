0f3fffa5a908599b249f2de854f7c0db2802f36f
Petko <petkotihomirovpetkov@gmail.com>
2018-01-17 15:39:39 +0200

    Submitting second solution for Actions which gives 100 points

---
+++ DSAWorkshop/DSAWorkshop/Actions/Program.cs
@@ -0,0 +1,229 @@
+﻿//Solution One - LinkedList - 100 points
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Actions
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] arrayOfInts = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToArray();
+//            LinkedList<int> linkedList = new LinkedList<int>();
+//            Dictionary<int, LinkedListNode<int>> dictionary = new Dictionary<int, LinkedListNode<int>>();
+
+//            for (int i = 0; i < arrayOfInts[0]; i++)
+//            {
+//                dictionary.Add(i, new LinkedListNode<int>(i));
+//                linkedList.AddLast(dictionary[i]);
+//            }
+
+//            int[] currentPriority;
+//            int firstElement;
+//            int secondElement;
+
+//            for (int i = 0; i < arrayOfInts[1]; i++)
+//            {
+//                currentPriority = Console.ReadLine().Split().Select(Int32.Parse).ToArray();
+//                firstElement = currentPriority[0];
+//                secondElement = currentPriority[1];
+
+//                bool needChanges = true;
+
+//                LinkedListNode<int> currNode = dictionary[secondElement];
+//                while (currNode != null)
+//                {
+//                    if (currNode.Value == firstElement)
+//                    {
+//                        needChanges = false;
+//                        break;
+//                    }
+//                    currNode = currNode.Previous;
+//                }
+
+//                if (needChanges)
+//                {
+//                    LinkedListNode<int> elementForReplacing = dictionary[secondElement];
+//                    linkedList.Remove(elementForReplacing);
+
+//                    LinkedListNode<int> border = dictionary[firstElement];
+//                    while (border.Next != null && border.Next.Value < elementForReplacing.Value)
+//                    {
+//                        border = border.Next;
+//                    }
+//                    linkedList.AddAfter(border, elementForReplacing);
+//                }
+//            }
+
+//            foreach (var item in linkedList)
+//            {
+//                Console.WriteLine(item);
+//            }
+
+//        }
+//    }
+//}
+
+//Solution Two - Topological Sorting Simple Data Structures - 100 points
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Actions
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] arrayOfInts = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToArray();
+
+//            List<int>[] parents = new List<int>[arrayOfInts[0]];
+//            int[] children = new int[arrayOfInts[0]];
+//            bool[] visited = new bool[arrayOfInts[0]];
+
+//            int[] tempValues;
+//            int parent;
+//            int child;
+//            for (int i = 0; i < arrayOfInts[1]; i++)
+//            {
+//                tempValues = Console.ReadLine().Split().Select(Int32.Parse).ToArray();
+//                parent = tempValues[0];
+//                child = tempValues[1];
+
+//                if (parents[parent] == null)
+//                {
+//                    parents[parent] = new List<int>();
+//                }
+
+//                parents[parent].Add(child);
+//                children[child]++;
+//            }
+
+//            for (int i = 0; i < parents.Length; i++)
+//            {
+//                if (parents[i] == null)
+//                {
+//                    parents[i] = new List<int>();
+//                }
+//            }
+
+//            for (int i = 0; i < children.Length; i++)
+//            {
+//                if (children[i] == 0 && !visited[i])
+//                {
+//                    Console.WriteLine(i);
+//                    visited[i] = true;
+//                    for (int j = 0; j < parents[i].Count; j++)
+//                    {
+//                        int parentCurrentChild = parents[i][j];
+//                        children[parentCurrentChild]--;
+//                    }
+//                    i = -1;
+//                }
+//            }
+
+//        }
+//    }
+//}
+
+//Solution Two - Topological Sorting Simple Data Structures - 100 points
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Actions
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] arrayOfInts = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToArray();
+//            Node[] graph = new Node[arrayOfInts[0]];
+
+//            int[] currentInputLine;
+//            int parent;
+//            int child;
+
+//            for (int i = 0; i < arrayOfInts[1]; i++)
+//            {
+//                currentInputLine = Console.ReadLine().Split().Select(Int32.Parse).ToArray();
+//                parent = currentInputLine[0];
+//                child = currentInputLine[1];
+
+//                if (graph[parent] == null)
+//                {
+//                    graph[parent] = new Node(child);
+//                    if (graph[child] == null)
+//                    {
+//                        graph[child] = new Node();
+//                    }
+//                    graph[child].Parents.Add(parent);
+//                }
+//                else
+//                {
+//                    graph[parent].Children.Add(child);
+//                    if (graph[child] == null)
+//                    {
+//                        graph[child] = new Node();
+//                    }
+//                    graph[child].Parents.Add(parent);
+//                }
+//            }
+
+//            for (int i = 0; i < graph.Length; i++)
+//            {
+//                if (graph[i] == null)
+//                {
+//                    graph[i] = new Node();
+//                }
+//            }
+
+//            for (int i = 0; i < graph.Length; i++)
+//            {
+//                if (graph[i].Parents.Count == 0 && !graph[i].IsVisited)
+//                {
+//                    Console.WriteLine(i);
+//                    graph[i].IsVisited = true;
+//                    for (int j = 0; j < graph.Length; j++)
+//                    {
+//                        graph[j].Parents = graph[].Parents.Where(currentParent => currentParent != i).ToList();
+//                    }
+//                    i = -1;
+//                }
+//            }
+//        }
+//    }
+
+//    class Node
+//    {
+//        private List<int> children;
+//        private List<int> parents;
+
+//        public Node()
+//        {
+//            this.children = new List<int>();
+//            this.parents = new List<int>();
+//        }
+
+//        public Node(int child)
+//        {
+//            this.children = new List<int>();
+//            this.parents = new List<int>();
+//            this.children.Add(child);
+//        }
+
+//        public List<int> Children { get => this.children;  set => this.children = value; }
+//        public List<int> Parents { get => this.parents; set => this.parents = value; }
+//        public bool IsVisited { get; set; }
+//    }
+//}
---
+++ DSAWorkshop/DSAWorkshop/LargestAreaInMatrixDFSIterating/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestAreaInMatrixDFSIterating
+{
+    class Program
+    {
+        private static int[,] graph;
+        private static bool[,] booleanGraph;
+        private static Stack<Vertex> stack;
+        private static int[] rowDirections = new int[] { 0, -1, 0, 1 };
+        private static int[] colDirections = new int[] { -1, 0, 1, 0 };
+        private static int bestCount = 0;
+        private static int currentCount = 0;
+
+        static void Main(string[] args)
+        {
+            int[] inputLine = Console.ReadLine().Split().Select(Int32.Parse).ToArray();
+            graph = new int[inputLine[0], inputLine[1]];
+            booleanGraph = new bool[inputLine[0], inputLine[1]];
+
+            for (int row = 0; row < graph.GetLength(0); row++)
+            {
+                inputLine = Console.ReadLine().Split().Select(Int32.Parse).ToArray();
+                for (int col = 0; col < graph.GetLength(1); col++)
+                {
+                    graph[row, col] = inputLine[col];
+                }
+            }
+
+            stack = new Stack<Vertex>();
+
+            for (int row = 0; row < graph.GetLength(0); row++)
+            {
+                for (int col = 0; col < graph.GetLength(1); col++)
+                {
+                    Vertex currentVertex = new Vertex(row, col);
+                    IterativeDFS(currentVertex);
+                    bestCount = Math.Max(bestCount, currentCount);
+                    currentCount = 0;
+                }
+            }
+
+            Console.WriteLine(bestCount);
+        }
+
+        private static void IterativeDFS(Vertex currentVertex)
+        {
+            stack.Push(currentVertex);
+            while (stack.Count != 0)
+            {
+                currentVertex = stack.Pop();
+                currentCount++;
+                if ((!booleanGraph[currentVertex.Row, currentVertex.Col]))
+                {
+                    booleanGraph[currentVertex.Row, currentVertex.Col] = true;
+                    foreach (Vertex vertex in GetAdjacentVertex(currentVertex))
+                    {
+                        stack.Push(vertex);
+                    }
+                }
+            }
+        }
+
+        private static ICollection<Vertex> GetAdjacentVertex(Vertex currentVertex)
+        {
+            ICollection<Vertex> vertex = new List<Vertex>();
+            Vertex currentCandidate;
+            for (int i = 0; i < rowDirections.Length; i++)
+            {
+                currentCandidate = new Vertex(currentVertex.Row + rowDirections[i], currentVertex.Col + colDirections[i]);
+                if ((currentCandidate.Row > -1 && currentCandidate.Row < graph.GetLength(0) && currentCandidate.Col > -1 && currentCandidate.Col < graph.GetLength(1)) && graph[currentCandidate.Row, currentCandidate.Col] == graph[currentVertex.Row, currentVertex.Col] && !booleanGraph[currentCandidate.Row, currentCandidate.Col])
+                {
+                    vertex.Add(currentCandidate);
+                }
+            }
+            return vertex;
+        }
+    }
+
+    class Vertex
+    {
+        public Vertex(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+
+        public int Row { get; set; }
+        public int Col { get; set; }
+    }
+}
